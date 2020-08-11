85e2d54cc26c85442f0a8016ab4b9cea9ab0916d
Bruno Masci <bruno.masci@gmail.com>
2020-08-11 00:21:43 -0300

    ble

--- multiboot-checker.sh
+++ multiboot-checker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! grub-file --is-x86-multiboot $1; then
-	echo $1: Invalid Multiboot image! Aborting...
+if ! grub2-file --is-x86-multiboot $1; then
+	echo "$1": Invalid Multiboot image! Aborting...
 	exit 1
 fi
