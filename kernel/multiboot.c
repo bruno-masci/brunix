60436a84193c8aeeb2b454f16a7cc8a557867767
Bruno Masci <bruno.masci@gmail.com>
2020-09-04 16:08:47 -0300

    ble

--- kernel/multiboot.c
+++ kernel/multiboot.c
@@ -1,8 +1,8 @@
 /**
  * @author Bruno Masci
- * @brief Multiboot related functions
- *
- * This file contains the mapping from GRUB's Multiboot info.
+ * @brief Multiboot Standard related functions
+ * @see kernel/main.c
+ * @see kernel/multiboot_entry_point.S
  */
 
 
@@ -14,12 +14,22 @@
 #include <brunix/kernel.h>          // for panic()
 
 
+/**
+ * @brief Copy the multiboot info set by the bootloader to a location we manage.
+ *
+ * The bootloader fills a data structure (struct std_multiboot_info) with contextual information about the system.
+ * We can't trust its location (in this case, GRUB chooses that location) and maybe at some time we unintentionally overwrite
+ * it. It is better to move that to a location we manage!
+ */
 void save_multiboot_info(struct std_multiboot_info *std_mboot_info_ptr, struct multiboot_info *mboot_info_ptr) {
     strncpy(mboot_info_ptr->cmdline, (const char *) std_mboot_info_ptr->cmdline, MAX_CMDLINE_LEN);
     mboot_info_ptr->mem_upper = std_mboot_info_ptr->mem_upper;
     mboot_info_ptr->flags = std_mboot_info_ptr->flags;
 }
 
+/**
+ * @brief Verify this kernel was loaded by a Multiboot-compliant bootloader.
+ */
 void verify_loader(uint32_t magic) {
     if (magic != MBOOT_LOADER_MAGIC) {
         panic("Invalid magic number! A Multiboot-compliant loader is needed...");
