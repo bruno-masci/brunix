a2b8ccc9fbd2d4172923c4919f5e65bb57f1c982
Bruno Masci <bruno.masci@gmail.com>
2020-09-12 18:19:02 -0300

    ble

--- include/arch/x86/memlayout.h
+++ include/arch/x86/memlayout.h
@@ -2,16 +2,17 @@
  * @author Bruno Masci
  * @brief x86 memory layout
  * @see /linker.ld.pp
+ * @see http://wiki.osdev.org/Memory_Map_(x86)
  */
 
 #ifndef __ARCH_MEMLAYOUT_H__
 #define __ARCH_MEMLAYOUT_H__
 
 
-#define EXT_MEM_BASE 0x00100000                 // Start of physical "extended" memory (1 MiB)
+#define EXT_MEM_BASE    0x00100000                      // Start of physical "extended" memory (1 MiB)
 
-#define KERN_BASE 0x00000000                    // First kernel virtual address
-#define KERN_LINK (KERN_BASE + EXT_MEM_BASE)    // Address where kernel is linked at
+#define KERN_BASE       0x00000000                      // First kernel virtual address
+#define KERN_LINK       (KERN_BASE + EXT_MEM_BASE)      // Address where kernel is linked at
 
 
 #endif /* __ARCH_MEMLAYOUT_H__ */
--- linker.ld.pp
+++ linker.ld.pp
@@ -1,23 +1,35 @@
-/* Simple linker script for the kernel.
- * See the GNU ld manual to learn the syntax.
+/**
+ * @author Bruno Masci
+ * @brief Simple linker script for the kernel
+ * @see /kernel/multiboot_entry_point.S
+ * @see http://wiki.osdev.org/Multiboot
+ * @see http://wiki.osdev.org/LD
+ * @see http://wiki.osdev.org/GCC_Cross-Compiler
+ *
+ * This file is actually a template for the linker script. We apply the C preprocessor on this file so we can include
+ * some constants (instead of duplicating them) and use C-style comments.
+ * The ".pp" suffix in this file name is for "PreProcessor".
  */
 
-/* Note that we don't specify neither OUTPUT_FORMAT nor OUTPUT_ARCH commands
- * here (see http://wiki.osdev.org/GCC_Cross-Compiler)
- */
 
 #include <arch/x86/memlayout.h>     // for KERN_LINK, KERN_BASE
 
-/* This declares the symbol where kernel execution begin */
-/* ahora este simbolo se refiere a la dir virtual higher half */
+
+/* <<< DISCLAIMER >>>
+ * We don't specify neither OUTPUT_FORMAT nor OUTPUT_ARCH commands here (see http://wiki.osdev.org/GCC_Cross-Compiler)
+ */
+
+
+// This declares the symbol where kernel execution begin (see /kernel/multiboot_entry_point.S file)
 ENTRY(_start)
 
-. = KERN_LINK;     /* "." means the current address */
+// "." means the current address
+. = KERN_LINK;
 
 SECTIONS {
 
     .boot : {
-        /* Ensure that the multiboot header is at the beginning */
+        // Ensure that the multiboot header is at the beginning of the generated image
         *(.multiboot_header)
     }
 
@@ -25,12 +37,9 @@ SECTIONS {
         *(.init)
     }
 
-    PROVIDE(kernel_start = .);
-
     .text ALIGN(4096) : AT (ADDR(.text) - KERN_BASE) {
 	    *(.text .stub .text.* .gnu.linkonce.t.*)
     }
-    PROVIDE(etext = .);
 
     .rodata ALIGN(4096) : AT (ADDR(.rodata) - KERN_BASE) {
         *(.rodata .rodata.* .gnu.linkonce.r.*)
@@ -44,8 +53,6 @@ SECTIONS {
     .data ALIGN(4096) : AT (ADDR(.data) - KERN_BASE) {
     	*(.data)
     }
-
-	PROVIDE(edata = .);
 
     .bss ALIGN(4096) : AT (ADDR(.bss) - KERN_BASE) {
         *(.bss)
